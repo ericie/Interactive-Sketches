b3108fcfd9d4dfd36de9c30bf6583c5667ae938d
Eric Ishii Eckhardt <eric@adaptedstudio.com>
2012-02-24 19:22:40 -0500

    Added spider tests
    Signed-off-by: Eric Ishii Eckhardt <eric@adaptedstudio.com>

---
+++ projects/spiders/e_netlines_3/lib/main.js
@@ -0,0 +1,7 @@
+//require(["jquery", "jquery.alpha", "jquery.beta"], function($) {
+require(['toxi/geom/Vec2D', toxi/color/TColor], function(Vec2D, TColor){
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(function() {
+        $('body').alpha().beta();
+    });
+});
---
+++ projects/spiders/e_netlines_3/lib/main4.js
@@ -0,0 +1,5 @@
+//require(["jquery", "jquery.alpha", "jquery.beta"], function($) {
+require(['toxi/geom/Vec2D', toxi/color/TColor, color, geom, math, physics2d, processing, THREE, utils], function(Vec2D, TColor){
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    console.log("Loaded");
+});
